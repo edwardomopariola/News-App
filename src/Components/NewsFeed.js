a7fc512e461a7d83368a83e76707068cd56fbbcb
Edward Omopariola <femiedjr@gmail.com>
2025-04-12 08:40:14 -0400

    clean up extra lines

--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -4,58 +4,57 @@ import "./NewsFeed.css"; // Importing CSS for styling the component
 
 
 function NewsFeed({ searchQuery }) { // The NewsFeed component takes a query prop to fetch news articles based on the search query
-    const [articles, setArticles] = useState([]); // State to hold the news articles
-    const [isLoading, setisLoading] = useState(false); // State to indicate loading status
-    const [error, setError] = useState(null); // State to hold any error messages
+  const [articles, setArticles] = useState([]); // State to hold the news articles
+  const [isLoading, setisLoading] = useState(false); // State to indicate loading status
+  const [error, setError] = useState(null); // State to hold any error messages
 
-    useEffect(() => {
-      const getNews = async () => {
-        setisLoading(true); // Set loading status to true before fetching data
-        // setError(null); // Reset error state before fetching data
-        let data;
-        if (searchQuery) { // Check if there is a search query
-          data = await fetchNewsByQuery(searchQuery); // Fetch news articles based on the search query
-        } else {
-            data = await fetchTopNews(); // Fetch top news articles if no query is provided
-        }
-        if (!data) {
-          setError("No articles found"); // Set error message if no data is returned
-        } else {
-          setArticles(data); // Update the articles state with the fetched data
-          setError(null); // Reset error state if data is fetched successfully
-        }
-        setisLoading(false); // Set loading status to false after fetching data
-      };
+  useEffect(() => {
+    const getNews = async () => {
+      setisLoading(true); // Set loading status to true before fetching data
+      // setError(null); // Reset error state before fetching data
+      let data;
+      if (searchQuery) { // Check if there is a search query
+        data = await fetchNewsByQuery(searchQuery); // Fetch news articles based on the search query
+      } else {
+        data = await fetchTopNews(); // Fetch top news articles if no query is provided
+      }
+      if (!data) {
+        setError("No articles found"); // Set error message if no data is returned
+      } else {
+      setArticles(data); // Update the articles state with the fetched data
+        setError(null); // Reset error state if data is fetched successfully
+      }
+      setisLoading(false); // Set loading status to false after fetching data
+    };
 
-      getNews(); // Call the function to fetch news articles
-    }, [searchQuery]); // Dependency array to re-run the effect when the query changes
-    // console.log("Query received in NewsFeed:", query);
+    getNews(); // Call the function to fetch news articles
+  }, [searchQuery]); // Dependency array to re-run the effect when the query changes
+  // console.log("Query received in NewsFeed:", query);
 
-    // condition to check if data is still loading or if there is an error
-    if (isLoading) {
-        return <div>Loading...</div>; // Show loading message while fetching data
-    }
-    if (error) {
-        return <div>Error: {error}</div>; // Show error message if there is an error
-    }
+  // condition to check if data is still loading or if there is an error
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading message while fetching data
+  }
+  if (error) {
+   return <div>Error: {error}</div>; // Show error message if there is an error
+  }
 
-    return (
-        <div className="news-feed"> {/* Container for the news articles */}
-            <h1 className="h1"> Top-New</h1>
-            {articles.map((article, index) => ( // Map through the articles and render each one
-                <div key={index} className="article"> {/* Unique key for each article */}
-                  <h1 className="article-title">{article.title}</h1> {/* Article title */}
-                  <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
-                  <h2 className="article-source-name">{article.source.name}</h2> {/* Article source name */}
-                  <p className="article-published">{article.publishedAt}</p>  {/* Article published date */}
-                  <p className="article-author">{article.author}</p> {/* Article author */}
-                  <p className="article-description">{article.description}</p> {/* Article description */}
-                  <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
-                </div>
-            ))}
+  return (
+    <div className="news-feed"> {/* Container for the news articles */}
+      <h1 className="h1"> Top-New</h1>
+      {articles.map((article, index) => ( // Map through the articles and render each one
+        <div key={index} className="article"> {/* Unique key for each article */}
+          <h1 className="article-title">{article.title}</h1> {/* Article title */}
+          <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
+          <h2 className="article-source-name">{article.source.name}</h2> {/* Article source name */}
+          <p className="article-published">{article.publishedAt}</p>  {/* Article published date */}
+          <p className="article-author">{article.author}</p> {/* Article author */}
+          <p className="article-description">{article.description}</p> {/* Article description */}
+          <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
-
 
 export default NewsFeed; // Export the NewsFeed component for use in other parts of the application
--- src/Components/PopularNews.js
+++ src/Components/PopularNews.js
@@ -4,59 +4,59 @@ import "./PopularNews.css"; // Importing CSS for styling the component
 
 
 function PopularNews() { // This component fetches and displays popular news articles
-    const [popularNews, setPopularNews] = useState([]);   // State to hold the popular news articles
-    const [isLoading, setIsLoading] = useState(false);  // State to indicate loading status
-    const [error, setError] = useState(null);   // State to hold any error messages
+  const [popularNews, setPopularNews] = useState([]);   // State to hold the popular news articles
+  const [isLoading, setIsLoading] = useState(false);  // State to indicate loading status
+  const [error, setError] = useState(null);   // State to hold any error messages
 
-    useEffect(() => {
-      const fetchPopularNews = async () => {
-        setIsLoading(true); // Set loading status to true before fetching data
-        setError(null); // Reset error state before fetching data
-        const response = await axios.get("https://newsapi.org/v2/everything", {  // Fetch news articles based on the search query
-          params: {
-            q: 'query', // Search query for popular news articles
-            from: '2025-04-11', // Start date for the news articles
-            to: '2025-04-12', // End date for the news articles
-            sortBy: 'popularity', // Sort the articles by popularity
-            apiKey: '9d3f0e550db9405eaef87d257bca0a90', // API key for authentication
-          },
-        });
-        setPopularNews(response.data.articles); // Update the popular news state with the fetched data
+  useEffect(() => {
+    const fetchPopularNews = async () => {
+      setIsLoading(true); // Set loading status to true before fetching data
+      setError(null); // Reset error state before fetching data
+      const response = await axios.get("https://newsapi.org/v2/everything", {  // Fetch news articles based on the search query
+        params: {
+          q: 'query', // Search query for popular news articles
+          from: '2025-04-11', // Start date for the news articles
+          to: '2025-04-12', // End date for the news articles
+          sortBy: 'popularity', // Sort the articles by popularity
+          apiKey: '9d3f0e550db9405eaef87d257bca0a90', // API key for authentication
+        },
+      });
+      setPopularNews(response.data.articles); // Update the popular news state with the fetched data
       
-        setIsLoading(false); // Set loading to false after fetching data
-      }
-
-      fetchPopularNews(); // Call the function to fetch popular news articles
-    }, []); // Empty dependency array to run the effect only once on component mount
-
-    // condition to check if data is still loading or if there is an error
-    if (isLoading) {
-      return <div>Loading...</div>; // Show loading message while fetching data
-    }
-    if (error) {
-      return <div>Error: {error}</div>; // Show error message if there is an error
+      setIsLoading(false); // Set loading to false after fetching data
     }
 
-    return (
-      <div className="popular-news"> {/* Container for popular news articles */}
-        <h1 className="h1">Popular News</h1> {/* Title for popular news section */}
-          {popularNews.length > 0 ? (
-            popularNews.map((article, index) => ( // Map through the popular news articles and render each one
-              <div key={index} className="article"> {/* Unique key for each article */}
-                <h3 className="article-title">{article.title}</h3> {/* Article title */}
-                <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
-                <h4 className="article-source-name">{article.source.name}</h4> {/* Article source name */}
-                <p className="article-published">{new Date(article.publishedAt).toLocaleDateString()}</p>  {/* Article published date */}
-                <p className="article-author">{article.author}</p> {/* Article author */}
-                <p className="article-description">{article.description}</p> {/* Article description */}
-                <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
-              </div>
-            ))
-          ) : (
+    fetchPopularNews(); // Call the function to fetch popular news articles
+  }, []); // Empty dependency array to run the effect only once on component mount
+
+  // condition to check if data is still loading or if there is an error
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading message while fetching data
+  }
+  if (error) {
+    return <div>Error: {error}</div>; // Show error message if there is an error
+  }
+
+  return (
+    <div className="popular-news"> {/* Container for popular news articles */}
+      <h1 className="h1">Popular News</h1> {/* Title for popular news section */}
+        {popularNews.length > 0 ? (
+          popularNews.map((article, index) => ( // Map through the popular news articles and render each one
+            <div key={index} className="article"> {/* Unique key for each article */}
+              <h3 className="article-title">{article.title}</h3> {/* Article title */}
+              <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
+              <h4 className="article-source-name">{article.source.name}</h4> {/* Article source name */}
+              <p className="article-published">{new Date(article.publishedAt).toLocaleDateString()}</p>  {/* Article published date */}
+              <p className="article-author">{article.author}</p> {/* Article author */}
+              <p className="article-description">{article.description}</p> {/* Article description */}
+              <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
+            </div>
+          ))
+        ) : (
           <div>No popular news articles found</div> // Show message if no popular news articles are found
         )}
-      </div>
-    );
+    </div>
+  );
 }
 
 export default PopularNews;
