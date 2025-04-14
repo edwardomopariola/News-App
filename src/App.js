6fa588fa8f2bf93ac8bc824b79f434034a426f85
Edward Omopariola <femiedjr@gmail.com>
2025-04-14 15:08:39 -0400

    changing the styling using css grid layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import SearchNews from './Components/SearchNews';
 import NewsFeed from './Components/NewsFeed';
-import Skeleton from './Components/Skeleton';
 import PopularNews from './Components/PopularNews';
+import './index.css'; // Importing CSS for styling the application
 
 
 function App() {
@@ -13,12 +13,13 @@ function App() {
   };
 
   return (
-    <div className='container'>
-      <h1 className='welcome-message'> Welcome to the news of the day!!!</h1>
-      <SearchNews onSearch={handleSearch} /> {/* Pass the handleSearch function to the SearchNews component */}
-      <NewsFeed searchQuery={searchQuery} /> {/* Pass the search query to the NewsFeed component */}
+    <div className='container'> {/* Main container for the application */}
+      <div className='header'><h2>News App</h2> {/* Header for the application */}
+        <h1 className='welcome-message'> Welcome to the news of the day!!!</h1>
+        <SearchNews onSearch={handleSearch} /> {/* Pass the handleSearch function to the SearchNews component */}
+      </div>
       <PopularNews /> {/* Render the PopularNews component */}
-      <Skeleton times={5} className="h-10 w-full" /> {/* Show skeleton loading effect */}    
+      <NewsFeed searchQuery={searchQuery} /> {/* Pass the search query to the NewsFeed component */}
     </div>
   );
 }
--- src/Components/NewsFeed.js
+++ src/Components/NewsFeed.js
@@ -40,19 +40,21 @@ function NewsFeed({ searchQuery }) { // The NewsFeed component takes a query pro
   }
 
   return (
-    <div className="news-feed"> {/* Container for the news articles */}
-      <h1 className="h1"> Top-New</h1>
-      {articles.map((article, index) => ( // Map through the articles and render each one
-        <div key={index} className="article"> {/* Unique key for each article */}
-          <h1 className="article-title">{article.title}</h1> {/* Article title */}
-          <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
-          <h2 className="article-source-name">{article.source.name}</h2> {/* Article source name */}
-          <p className="article-published">{article.publishedAt}</p>  {/* Article published date */}
-          <p className="article-author">{article.author}</p> {/* Article author */}
-          <p className="article-description">{article.description}</p> {/* Article description */}
-          <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
-        </div>
-      ))}
+    <div className="news-feed-container"> {/* Main container for the news articles */}
+      <div className="news-feed"> {/* Container for the news articles */}
+        <h1 className="h1"> Top-New</h1>
+        {articles.map((article, index) => ( // Map through the articles and render each one
+          <div key={index} className="article"> {/* Unique key for each article */}
+            <h1 className="article-title">{article.title}</h1> {/* Article title */}
+            <img className="article-image" src={article.urlToImage} alt={article.title} /> {/* Article image */}
+            <h2 className="article-source-name">{article.source.name}</h2> {/* Article source name */}
+            <p className="article-published">{article.publishedAt}</p>  {/* Article published date */}
+            <p className="article-author">{article.author}</p> {/* Article author */}
+            <p className="article-description">{article.description}</p> {/* Article description */}
+            <a className="article-link" href={article.url} target="_blank" rel="noopener noreferrer">Read more</a> {/* Link to read more */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
